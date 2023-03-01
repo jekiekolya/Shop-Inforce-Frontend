e3bb1ad0bcb4d4dc3693b7838b8e2afbcdc9d64c
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-01 18:00:58 +0200

    base fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/Templates-REACT">
+  <BrowserRouter basename="/Inforce-Shop">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
