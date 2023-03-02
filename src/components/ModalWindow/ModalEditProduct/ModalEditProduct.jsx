216a8dda4aae2ce6d6eb15ca4f811425ca6c4cca
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-02 15:19:01 +0200

    Update ModalEditProduct.jsx

--- src/components/ModalWindow/ModalEditProduct/ModalEditProduct.jsx
+++ src/components/ModalWindow/ModalEditProduct/ModalEditProduct.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { toggleShowModalEditProduct } from 'redux/modal/modalSlice';
+// import { toggleShowModalEditProduct } from 'redux/modal/modalSlice';
 
-// Components
-import ModalUniversal from '../ModalUniversal/ModalUniversal';
-import EditProductForm from 'components/EditProductForm/EditProductForm';
+// // Components
+// import ModalUniversal from '../ModalUniversal/ModalUniversal';
+// import EditProductForm from 'components/EditProductForm/EditProductForm';
 
-// Styles
-import s from './ModalEditProduct.module.scss';
+// // Styles
+// import s from './ModalEditProduct.module.scss';
 
 export default function ModalEditProduct({ product }) {
   return (
-    <ModalUniversal toggleAction={toggleShowModalEditProduct}>
-      <h2 className={s.title}>Edit Product</h2>
-      <EditProductForm product={product} />
-    </ModalUniversal>
+    <div>sasd</div>
+    // <ModalUniversal toggleAction={toggleShowModalEditProduct}>
+    //   <h2 className={s.title}>Edit Product</h2>
+    //   <EditProductForm product={product} />
+    // </ModalUniversal>
   );
 }
